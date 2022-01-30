ec69916714beefd537d7fe2d1aedd2729e7b26a5
Megi <mkceva@gmail.com>
2022-01-30 03:21:36 +0100

    defining resttemplate

--- src/main/java/com/example/consumerestfulwebservice/ConsumeRestfulWebServiceApplication.java
+++ src/main/java/com/example/consumerestfulwebservice/ConsumeRestfulWebServiceApplication.java
@@ -2,10 +2,17 @@ package com.example.consumerestfulwebservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ConsumeRestfulWebServiceApplication {
 
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder){
+		return builder.build();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumeRestfulWebServiceApplication.class, args);
 	}
